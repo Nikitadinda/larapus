566b9678508093dfdc1e37e669132e376afae56f
Nikita <nktdinda@gmail.com>
2021-11-13 16:46:41 +0700

    Develop Fitur Admin - CRUD BUKU

---
+++ app/Models/Book.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+class Book extends Model
+{
+    use HasFactory;
+    protected $visible = ['title', 'author_id', 'amount', 'cover'];
+    protected $fillable = ['title', 'author_id', 'amount', 'cover'];
+    public $timestamps = true;
+    public function author()
+    {
+        // data dari Model "Book" bisa di miliki oleh model "Author"
+        // melalui fk "author_id"
+        return $this->belongsTo('App\Models\Author', 'author_id');
+    }
+
+    public function image()
+    {
+        if ($this->cover && file_exists(public_path('images/books/' . $this->cover))) {
+            return asset('images/books/' . $this->cover);
+        } else {
+            return asset('images/no_image.png');
+        }
+    }
+
+    public function deleteImage()
+    {
+        if ($this->cover && file_exists(public_path('images/books/' . $this->cover))) {
+            return unlink(public_path('images/books/' . $this->cover));
+        }
+
+    }
+
+}
